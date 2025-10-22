49ba4241ab62c073dcec3818f28d7dce25e98b53
Andre21ac <andrecristovao15@gmail.com>
2025-10-22 17:05:19 -0300

    add new logic for menu hamburguer

--- assets/js/menuController.js
+++ assets/js/menuController.js
@@ -2,9 +2,5 @@ const button = document.getElementById('menu-button');
 const menu = document.getElementById('menu-hamburguer');
 
 button.addEventListener("click", function (e){
-    if(menu.style.display === "none")
-        menu.style.display = "block";
-
-    else
-         menu.style.display = "none";
+   menu.style.display === "block" ? menu.style.display = "none" : menu.style.display = "block";
 })
